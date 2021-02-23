0d1c0a205be4656c3d8d359039edabf76983d2a8
Robert Wagner <rw@20sec.net>
2021-02-23 17:07:11 +0100

    leeres excerpt

--- web/src/components/PostCard.vue
+++ web/src/components/PostCard.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="post-card__content">
       <h2 class="post-card__title" v-html="post.title" />
-      <block-content :blocks="post._rawExcerpt" />
+      <block-content v-if="post._rawExcerpt" :blocks="post._rawExcerpt" />
 
       <post-meta class="post-card__meta" :post="post" />
       <post-tags class="post-card__tags" :post="post" />
