4022aede51584e726996d70dd63c364a87878541
rw <rw@20sec.net>
2021-02-24 21:41:33 +0100

    reservierte tage

--- web/src/pages/Calendar.vue
+++ web/src/pages/Calendar.vue
@@ -20,6 +20,7 @@
             :showDropdowns=true
             :autoApply=false
             :closeOnEsc=false
+				:date-format="mark_reserved"
             v-model="dateRange"
             @update="updateValues"
             @toggle="checkOpen" 
@@ -46,6 +47,8 @@
 </page-query>
 
 <script>
+import { eachDayOfInterval, format } from 'date-fns'
+
 import AuthorCard from '~/components/AuthorCard'
 import DateRangePicker from 'vue2-daterange-picker'
 
@@ -61,6 +64,7 @@ export default {
   },
   data () {
       return {
+		  reserved: {},
         minDate: "2021-02-01",
         maxDate: "2024-06-30",
         opens: true,
@@ -76,7 +80,39 @@ export default {
     },
     checkOpen(){
       return true
-    }
+    },
+	 mark_reserved(classes, date){
+		if (!classes.disabled) {
+			var d = format(date, 'yyyy/MM/dd')
+			if(this.reserved[d] && this.reserved[d]==2) classes.disabled=true
+		}
+		return classes
+	 },
+	 setup_reserved(resv){
+		var me = this
+		var days = resv.map(function(e){
+			console.log("el", e, new Date(e[0]), new Date(e[1]))
+			return eachDayOfInterval({start: new Date(e[0]), end: new Date(e[1])})
+		})
+		console.log(days)
+		days.forEach(function(els, i){
+			var max = els.length-1
+			els.forEach(function(e, j){
+				var weight=(j==0 || j==max)?1:2
+				var d = format(e, 'yyyy/MM/dd')
+				if(me.reserved[d]) me.reserved[d]++
+				else me.reserved[d]=weight
+			})
+
+		})
+		console.log("res days", this.reserved)
+	}
+  },
+	mounted: function () {
+    var reserved = [['2021/03/14', '2021/03/17'], ['2021/03/19', '2021/03/21' ], ['2021/03/22', '2021/03/25'], ['2021/03/25', '2021/03/28']]
+	 this.setup_reserved(reserved)
+    console.log('mounted', reserved)
+	
   }
 }
 </script>
