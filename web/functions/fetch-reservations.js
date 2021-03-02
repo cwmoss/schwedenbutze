46eabb70adb304ae2c98be91958531127448c554
rw <rw@20sec.net>
2021-03-02 23:53:18 +0100

    netlify function versuch1

---
+++ web/functions/fetch-reservations.js
@@ -0,0 +1,43 @@
+/*
+npm install netlify-cli -g
+netlify dev
+http://localhost:8888/.netlify/functions/fetch-reservations
+*/
+
+const sanityClient = require('@sanity/client')
+const client = sanityClient({
+  projectId: 'emjk7lsc',
+  dataset: 'production',
+  token: '', //process.env.S_TOKEN||'', // or leave blank to be anonymous user
+  useCdn: true // `false` if you want to ensure fresh data
+})
+
+const now = new Date()
+const fnow = [
+	now.getFullYear(),
+	("0" + (now.getMonth() + 1)).slice(-2),
+	("0" + now.getDate()).slice(-2)
+	]
+	.join('-')
+
+const query = '*[_type == "reservation" && departure >= $now] {arrival, departure}'
+const params = {now: fnow}
+
+exports.handler = async function(event, context) {
+	
+	var rs
+	
+	return client.fetch(query, params)
+		.then(reservations => {
+			console.log("results")
+		  reservations.forEach(r => {
+		   		console.log(`reserved from ${r.arrival} to ${r.departure}`)
+		  	})
+		return {
+	        statusCode: 200,
+	        body: JSON.stringify({rsrv: reservations})
+	    };
+	})
+	
+    
+}
