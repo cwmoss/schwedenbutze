0ea97d503b1bcfb3696ea118707621b8ae553d9c
Slick Jurgen <slickjurgen@googlemail.com>
2021-05-02 19:24:35 +0200

    Load intro for / page

--- web/src/pages/Index.vue
+++ web/src/pages/Index.vue
@@ -1,6 +1,29 @@
 <template>
   <Layout :show-logo="false">
+    <div class="post-title">
+      <h1 class="post-title__text">{{ $page.post.title }}</h1>
 
+    </div>
+
+    <div class="post content-box">
+      <div class="post__header">
+        <img
+          alt="Cover image"
+          v-if="$page.post.mainImage"
+          :src="$urlForImage($page.post.mainImage, $page.metadata.sanityOptions).width(600).auto('format').url()"
+        />
+      </div>
+
+      <block-content
+        class="post__content"
+        :blocks="$page.post._rawBody"
+        v-if="$page.post._rawBody"
+      />
+
+      <div class="post__footer">
+        <post-tags :post="$page.post" v-if="$page.post" />
+      </div>
+    </div>
   </Layout>
 </template>
 
@@ -12,25 +35,33 @@
       dataset
     }
   }
-  post: sanityPage (path: "/navigation") {
+  post: sanityPost (path: "/intro") {
     title
-    sections{
+    publishedAt (format: "D. MMMM YYYY")
+    categories {
+      id
       title
-      ref{
-        ... on SanityPost{
-        _type
+    }
+    _rawExcerpt
+    _rawBody
+    mainImage {
+      asset {
         _id
-        path
-          title
-          _rawBody(resolveReferences: {maxDepth: 5})
-          mainImage{
-            asset {
-              _id
-              url
-            }
-            caption
-          }
-        }
+        url
+      }
+      caption
+      alt
+      hotspot {
+        x
+        y
+        height
+        width
+      }
+      crop {
+        top
+        bottom
+        left
+        right
       }
     }
   }
@@ -39,18 +70,86 @@
 </page-query>
 
 <script>
-import AuthorCard from '~/components/AuthorCard'
-import PostCard from '~/components/PostCard'
-import Navigation from '~/components/Navigation'
+import BlockContent from '~/components/BlockContent'
+import PostMeta from '~/components/PostMeta'
+import PostTags from '~/components/PostTags'
 
 export default {
   components: {
-    AuthorCard,
-    PostCard,
-    Navigation
+    PostMeta,
+    PostTags,
+    BlockContent
   },
-  metaInfo: {
-    title: 'Välkommen till Zorros Südhof'
+  metaInfo() {
+    return {
+      title: this.$page.post.title,
+      meta: [
+        {
+          name: 'description',
+          content: this.$page.post.description
+        }
+      ]
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.post-title {
+  padding: calc(var(--space) / 2) 0 calc(var(--space) / 2);
+  text-align: center;
+}
+
+.post {
+
+  background-color: var(--bg-color);
+  
+  &__header {
+    width: calc(100% + var(--space) * 2);
+    margin-left: calc(var(--space) * -1);
+    margin-top: calc(var(--space) * -1);
+    margin-bottom: calc(var(--space) / 2);
+    overflow: hidden;
+    border-radius: var(--radius) var(--radius) 0 0;
+
+    img {
+      width: 100%;
+    }
+
+    &:empty {
+      display: none;
+    }
+  }
+
+  &__content {
+
+    h2:first-child {
+      margin-top: 0;
+    }
+
+    p:first-of-type {
+      font-size: 1em;
+      color: var(--body-color);
+    }
+
+    img {
+      width: calc(100% + var(--space) * 2);
+      margin-left: calc(var(--space) * -1);
+      display: block;
+      max-width: none;
+    }
+  }
+}
+
+.post-comments {
+  padding: calc(var(--space) / 2);
+
+  &:empty {
+    display: none;
+  }
+}
+
+.post-author {
+  margin-top: calc(var(--space) / 2);
+}
+</style>
